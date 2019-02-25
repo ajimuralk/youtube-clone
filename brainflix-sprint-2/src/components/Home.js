c213f4190413cce71d288579c7505e0d74574a2a
ajimura <ajimuralk@gmail.com>
2019-02-24 16:50:46 -0800

    refactored switchVideo func and get req

--- brainflix-sprint-2/src/components/Home.js
+++ brainflix-sprint-2/src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 import Nav from "./Nav";
 import Video from "./Video";
 import VideoInfo from "./VideoInfo";
@@ -8,7 +9,6 @@ import logo from "../BrainFlix-Sprint-1/4.0 - Assets/Logo/Logo-brainflix.png";
 import profilePicture from "../BrainFlix-Sprint-1/4.0 - Assets/Images/Mohan-muruge.jpg";
 import "../styles/App.css";
 import "../styles/global.css";
-import axios from "axios";
 
 const api_key = "?api_key=4edb152d-f186-4793-836c-d5befa07ea2f";
 const videosUrl = `https://project-2-api.herokuapp.com/videos/${api_key}`;
@@ -39,24 +39,13 @@ class Home extends Component {
       .then(res => {
         const propsId = this.props.match.params.id;
         const videoId = propsId ? propsId : defaultVideo;
-        axios
-          .get(mainVideoUrl(videoId))
-          .then(res => {
-            this.setState({
-              mainVideo: res.data,
-              commentList: res.data.comments
-            });
-          })
-          .catch(err => {
-            console.log(err);
-          });
+        this.switchVideo(videoId);
       });
   }
-  
-  switchVideo() {
-    const videoId = this.props.match.params.id;
+
+  switchVideo(id) {
     axios
-      .get(mainVideoUrl(videoId))
+      .get(mainVideoUrl(id))
       .then(res => {
         this.setState({
           mainVideo: res.data,
@@ -70,7 +59,6 @@ class Home extends Component {
 
   render() {
     if (this.state.mainVideo === null) return "Loading...";
-    console.log(this.state.mainVideo.id);
     return (
       <div>
         <Nav logo={logo} profilePicture={profilePicture} />
--- brainflix-sprint-2/src/components/NextVideo.js
+++ brainflix-sprint-2/src/components/NextVideo.js
@@ -5,7 +5,7 @@ class NextVideo extends Component {
   render() {
     return (
       <div className="NextVideo">
-        <Link className="NextVideo-link" to={"/videos/" + this.props.id} onClick={() => this.props.switchVideo()}>
+        <Link className="NextVideo-link" to={"/videos/" + this.props.id} onClick={() => this.switchVideo(this.props.id)}>
           <img src={this.props.image} alt="thumbnail" />
           <div className="NextVideo-info">
             <span className="NextVideo__title">{this.props.title}</span>
