f2e88583bc4c6807efc9190f1cfdebe975e833f4
ajimura <ajimuralk@gmail.com>
2019-02-27 16:24:37 -0800

    adding sprint-3

---
+++ brainflix-sprint-3/src/components/Home.js
@@ -0,0 +1,89 @@
+import React, { Component } from "react";
+import axios from "axios";
+import Nav from "./Nav";
+import Video from "./Video";
+import VideoInfo from "./VideoInfo";
+import NextVideoList from "./NextVideoList";
+import CommentsDisplay from "./CommentsDisplay";
+import logo from "../assets/Logo/Logo-brainflix.png";
+import profilePicture from "../assets/Images/Mohan-muruge.jpg";
+
+const api_key = "?api_key=4edb152d-f186-4793-836c-d5befa07ea2f";
+const videosUrl = `https://project-2-api.herokuapp.com/videos/${api_key}`;
+const defaultVideo = "1af0jruup5gu";
+
+const mainVideoUrl = id =>
+  `https://project-2-api.herokuapp.com/videos/${id}?api_key=${api_key}`;
+
+class Home extends Component {
+  state = {
+    videos: [],
+    commentList: [],
+    mainVideo: null
+  };
+
+  //VIDEO API
+  componentDidMount() {
+    axios
+      .get(videosUrl)
+      .then(res => {
+        this.setState({
+          videos: res.data
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      })
+      .then(res => {
+        const propsId = this.props.match.params.id;
+        const videoId = propsId ? propsId : defaultVideo;
+        this.switchVideo(videoId);
+      });
+  }
+
+  switchVideo(id) {
+    axios
+      .get(mainVideoUrl(id))
+      .then(res => {
+        this.setState({
+          mainVideo: res.data,
+          commentList: res.data.comments
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  render() {
+    if (this.state.mainVideo === null) return "Loading...";
+    return (
+      <div>
+        <Nav logo={logo} profilePicture={profilePicture} />
+        <Video
+          image={this.state.mainVideo.image}
+          video={this.state.mainVideo.video}
+          api_key={api_key}
+        />
+        <div className="more-content">
+          <div className="more-content__this-video">
+            <VideoInfo mainVideo={this.state.mainVideo} />
+            <CommentsDisplay
+              profilePicture={profilePicture}
+              userComments={this.state.commentList}
+            />
+          </div>
+          <aside className="more-content__next-videos">
+            <NextVideoList
+              sideVideo={this.state.videos}
+              currentVideoId={this.state.mainVideo.id}
+              switchVideo={this.switchVideo}
+            />
+          </aside>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
---
+++ brainflix-sprint-3/src/components/Video.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class Video extends Component {
+  render() {
+    const videoUrl = `${this.props.video}${this.props.api_key}`
+    
+    return (
+      <main className="Video">
+        <video
+          className="Video__player"
+          controls
+          autoPlay
+          src={videoUrl}
+        />
+      </main>
+    );
+  }
+}
+
+export default Video;
